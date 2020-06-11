b70b5d62ff1115cce5fc3c9b1d5e8fb95071cd83
srireddy27 <66113734+srireddy27@users.noreply.github.com>
2020-06-11 10:35:24 +0530

    Add files via upload

--- new.py
+++ new.py
@@ -1,34 +1,10 @@
-def longestRepeatedSubstring(str): 
-    n = len(str) 
-    LCSRe = [[0 for x in range(n + 1)]  
-                for y in range(n + 1)] 
-    res = "" 
-    res_length = 0
-    index = 0
-    for i in range(1, n + 1): 
-        for j in range(i + 1, n + 1): 
-            if (str[i - 1] == str[j - 1] and
-                LCSRe[i - 1][j - 1] < (j - i)): 
-                LCSRe[i][j] = LCSRe[i - 1][j - 1] + 1
-                if (LCSRe[i][j] > res_length): 
-                    res_length = LCSRe[i][j] 
-                    index = max(i, index) 
-            else: 
-                LCSRe[i][j] = 0
-    if (res_length > 0): 
-        for i in range(index - res_length + 1, 
-                                    index + 1): 
-            res = res + str[i - 1] 
-    return res 
-test_case=int(input())
-for _ in range(test_case):
-    n=input()
-    if(len(n)<=1):
-        print("No")
-    else:
-        value=longestRepeatedSubstring(n);
-        res=len(n)-2*len(value);
-        if(len(n)==res):
-            print("No")
-        else:
-            print("Yes")
+from collections import Counter
+t,n=map(int,input().split())
+strings=[list(input()) for i in range(t)]
+val=list(zip(*strings))
+res=[]
+for i in val:
+    ob=Counter(sorted(list(i)))
+    high_val=ob.most_common(1)
+    res.append(high_val[0][0])
+print("".join(res))
